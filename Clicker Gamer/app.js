bb2fe3354575051be7ca25c4d98784d0437eb92f
Maxwell Vivino <mvivino@Maxwells-MacBook.local>
2020-02-11 11:39:33 -0500

    Banana Game (No win condition)

--- Clicker Gamer/app.js
+++ Clicker Gamer/app.js
@@ -1,20 +1,23 @@
-function Setup(){
-    createCanvas(windowWidth, windowHeight )
-    bananaImg = createImg('banana.png', 'banana image')
-    bananaImg.size(200, 100)
-    heading = createElement('h2', 'Click The Banana!')  
-    heading.position(width / 3, height / 4)
-    frameRate(2)
+  let bananaImg
+  let heading
+  
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    bananaImg = createImg('banana.png', 'banana image');
+    bananaImg.size(200, 100);
+    heading = createElement('h2', 'Click The Banana!');
+    heading.position(width / 3, height / 4);
+    frameRate(2);
 }
 
-function Draw(){
-     bananaImg.position(random(width), random(height))
-
-     button = createButton(bananaImg)
-     button.mousePressed(youWon)
+function draw(){
+     bananaImg.position(random(width),random(height))
+     button = createButton(bananaImg);
+     button.mousePressed(youWon);
 }
 
-function youWon(){
-    heading.html('You Won!')
-    bananaImg.remove()
+function youWon() {
+    heading.html('You Won!');
+    bananaImg.remove();
 }
+
