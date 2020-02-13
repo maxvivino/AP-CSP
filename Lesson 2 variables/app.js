33d1fa7ecf07a5042b89730a996759021c88f257
Maxwell Vivino <mvivino@Maxwells-MacBook.local>
2020-02-13 13:47:03 -0500

    Lesson  2 variables

---
+++ Lesson 2 variables/app.js
@@ -0,0 +1,10 @@
+const add = 7 + 3;
+const subtract = 7 - 3;
+const multiply = 7 * 3;
+const divide = 7 / 3;
+const paren = (83 + 92 + 86 + 94) / 4
+console.log (add, subtract, multiply, divide)
+let score = 0;
+let lives = 3;
+console.log("You have " + lives + " lives left with a score of " + score);
+
