413302266b83cc816c84c817a38dbbe8d9441cb2
Maxwell Vivino <maxwellvivino@ORRHS.ORG>
2020-02-13 13:53:50 -0500

    Expressions with Variables

--- Lesson 2 variables/app.js
+++ Lesson 2 variables/app.js
@@ -1,10 +1,19 @@
-const add = 7 + 3;
-const subtract = 7 - 3;
-const multiply = 7 * 3;
-const divide = 7 / 3;
-const paren = (83 + 92 + 86 + 94) / 4
-console.log (add, subtract, multiply, divide)
-let score = 0;
-let lives = 3;
-console.log("You have " + lives + " lives left with a score of " + score);
+// const add = 7 + 3;
+// const subtract = 7 - 3;
+// const multiply = 7 * 3;
+// const divide = 7 / 3;
+// const paren = (83 + 92 + 86 + 94) / 4
+// console.log (add, subtract, multiply, divide)
+// let score = 0;
+// let lives = 3;
+// console.log("You have " + lives + " lives left with a score of " + score);
 
+let points = 1023;
+let lives = 3;
+let totalTime = 45;
+console.log("Points: " + points);
+console.log("Lives: " + lives);
+console.log("Total Time: " + totalTime + " secs");
+console.log("---------------------------");
+let totalScore = (points * lives) / totalTime ;
+console.log("Your Total Score is: " + totalScore);
