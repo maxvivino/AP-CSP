fae8dac343c83233e1cb1c826ddb57ec7c12e8b3
Maxwell Vivino <maxwellvivino@ORRHS.ORG>
2020-02-26 10:11:52 -0500

    Clicker Game v2

---
+++ Clicker Game v2/start.js
@@ -0,0 +1,13 @@
+let button
+let
+function setup(){
+  createCanvas(windowHeight,windowWidth)
+  const heading = createElement('h1', 'Welcome to Banana Clicker!')
+  heading.style('font-family: roboto')
+  heading.style('font-size: 50px')
+  heading.style('text-align: center')
+  heading.style('position: absolute')
+  heading.style('width: 100%')
+  heading.style('top: 100px')
+  button = createButton(Start, [value])
+}
