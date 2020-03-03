a8ebad9ece39d3e4413b4de9dfdf5ac99c34985e
Maxwell Vivino <mvivino@Maxwells-MacBook.local>
2020-03-03 09:05:36 -0500

    Working Game!

--- Clicker Game v2/lose.js
+++ Clicker Game v2/lose.js
@@ -1,11 +1,21 @@
 function setup(){
-    button=createButton('Restart')
+  createCanvas(windowHeight,windowWidth)
+  background(100,100,500)
+  const heading = createElement('h1', 'You Lose!!!')
+    heading.style('font-family: roboto')
+    heading.style('font-size: 50px')
+    heading.style('text-align: center')
+    heading.style('position: absolute')
+    heading.style('width: 100%')
+    heading.style('top: 100px')
+
+  button=createButton('Try Again')
    
     button.mousePressed(goToLink)
     button.style('font-size: 50px')
     button.style('background-color: red')
     button.style('width: 200px')
-    button.style('height: 100px')
+    button.style('height: 150px')
     button.position(windowWidth / 2.25, windowHeight / 1.25)
 }
 
--- Clicker Game v2/win.js
+++ Clicker Game v2/win.js
@@ -1,5 +1,6 @@
 function setup(){
-    
+  createCanvas(windowHeight,windowWidth)
+  background(100,100,500)
     const heading = createElement('h1', 'You Won!!!')
     heading.style('font-family: roboto')
     heading.style('font-size: 50px')
@@ -11,13 +12,13 @@ function setup(){
     trophyImg = createImg('Trophy.png', 'trophy image');
     trophyImg.size(300, 400);
     trophyImg.position(windowWidth / 2.4, windowHeight / 3.5)
-    button=createButton('Restart')
+    button=createButton('Play Again')
    
     button.mousePressed(goToLink)
     button.style('font-size: 50px')
     button.style('background-color: red')
     button.style('width: 200px')
-    button.style('height: 100px')
+    button.style('height: 150px')
     button.position(windowWidth / 2.25, windowHeight / 1.25)
 }
 
