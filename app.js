7776fdb0743c00227c283b732f687247bf1083ca
Maxwell Vivino <maxwellvivino@ORRHS.ORG>
2020-03-13 13:43:38 -0400

    Color Finder Game

--- app.js
+++ app.js
@@ -1,58 +1,70 @@
-// let time = 12
-// let greeting = ''
-
-// if(time < 12){
-//     greeting = 'Good Morning'
-// } else {
-//     if(time < 16){
-//     greeting = 'Good Afternoon'
-//     } else {
-//      greeting = 'Good Evening'
-// }
-// }
+/* eslint-disable no-eval */
+/* eslint-disable prefer-const */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-unused-vars */
 
 
-// function timeChecker(time){
-//     if(time < 12){
-//         greeting = 'Good Morning'
-//     } else if(time < 16){
-//         greeting = 'Good Afternoon'
-//         } else if(time < 20){
-//         greeting = 'Good Evening'
-//         } else {
-//             greeting = 'Good Night'
-//         }
-//     }
 
+function setup() {
+    let randButtonId
+    function checkCorrect (buttonId) {
+        console.log(`checking: ${buttonId}`)
+        setBoard()
+    }
+    
+        setBoard()
+    function setBoard() {
+    let randButtonId = 1
+    let currentPlayer = 1
+    let p1Score = 0
+    let p2Score = 0
+  
+    // Get UI Elements
+    const p1ScoreDisplay = select('#score1')
+    const p2ScoreDisplay = select('#score2')
+    const button1 = select('#button1')
+    const button2 = select('#button2')
+    const button3 = select('#button3')
+    const button4 = select('#button4')
+    //chooses random color
+    let R = random(0,255)
+    let G = random(0,255)
+    let B = random(0,255)
+    
+    let color= `RGB(${R}, ${G}, ${B})`
+    //makes different color 20 pixels different
+    G += 20
+    B += 20
+    R += 20
+    let diffColor = `RGB(${R}, ${G}, ${B})`
+   
+    //puts color to the buttons
+    button1.style('background-color', color)
+    button2.style('background-color', color)
+    button3.style('background-color', color)
+    button4.style('background-color', color) 
+    //Randomizes where the different color button is
+    randButtonId = `button${Math.floor(random(1,5))}`
+    select(`#${randButtonId}`).style('background-color', diffColor)
 
-// Logic Operators
-// let day = 'Wednesday'
+    console.log(`correct button is: ${randButtonId}`)
+    
+    button1.mousePressed(function () {
+        checkCorrect('button1')
+      })
 
-//     And
-// if(day === 'Wednesday' && day === 'Tuesday'){
-//     Some code
-// }
+    button2.mousePressed(function () {
+        checkCorrect('button2')
+      })
 
-//     Or
-// if(day === 'Wednesday' || day === 'Tuesday' || day === 'Saturday'){
-//     Some code
-// }
+    button3.mousePressed(function () {
+        checkCorrect('button3')
+      })
+    
+    button4.mousePressed(function () {
+        checkCorrect('button4')
+      })
 
-// Not
-let hasCar = true
-
-if(hasCar !== true){
-    console.log("You don't have a car")
-}
-//Same things
-if(hasCar === false){
-    console.log("You don't have a car")
-}
-
-if(hasCar){
-    console.log("You have a car")
-}
-
-if(!hasCar){
-    console.log("You don't have a car")
+  }
 }
