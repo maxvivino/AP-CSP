6510aeefacd719532e7fba6a0f69e812bf59831a
Maxwell Vivino <mvivino@Maxwells-MacBook.local>
2020-04-13 11:01:01 -0400

    Head Count complete

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 let headCount = 0
-
-while()
+let count = 0
+while(headCount < 12){
+    let coin = Math.floor(Math.random() * 2)
+   console.log(coin)
+    if(coin == 1){
+        headCount = headCount + 1
+    } else {
+        headCount = 0
+    }
+    console.log(`Head Streak: ${headCount}`)
+    //console.log (`Heads: ${headCount}`)
+    count++
+    //console.log(`Count: ${count}`)
+  } 
+  console.log(`Total Flips: ${count}`)
